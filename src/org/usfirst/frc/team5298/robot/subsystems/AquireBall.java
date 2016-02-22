d57d8dd7639fa88b7dfd0546a0d6a836fd2ccbff
ETechChargers5298 <frc5298@gmail.com>
2016-02-22 16:16:30 -0500

    I added the base methods into the Aquire

--- src/org/usfirst/frc/team5298/robot/subsystems/AquireBall.java
+++ src/org/usfirst/frc/team5298/robot/subsystems/AquireBall.java
@@ -1,14 +1,42 @@
 package org.usfirst.frc.team5298.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Talon;
+
+
+
 
 /**
  *
  */
+
 public class AquireBall extends Subsystem {
+	
+	private Talon Aquire;
+	
+	public AquireBall() {
+		
+		Aquire = new Talon();
+		
+	}
+	
+	public void Aquire() {
+		
+
+	}
+	
+	public void stop() {
+		
+		
+	}
+	
+	
+	
+	
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+	
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
