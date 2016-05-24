bf0ddbd9ca2a22b0bfbf8c4678e938cdff323730
jcosaluzzo <jcosaluzzo@icloud.com>
2016-05-24 16:06:57 -0400

    Replaced robotmap code with orignal robotmap code

--- src/org/usfirst/frc/team5298/robot/RobotMap.java
+++ src/org/usfirst/frc/team5298/robot/RobotMap.java
@@ -1,13 +1,17 @@
 package org.usfirst.frc.team5298.robot;
-/**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
+public interface RobotMap {
 public class RobotMap {
     // For example to map the left and right motors, you could define the
     // following variables to use with your drivetrain subsystem.
+	
+	public static int FrontLeftMotor = 1;
+    public static int FrontRightMotor = 2;
+    public static int RearLeftMotor = 3;
+    public static int RearRightMotor = 4;
+    public static int liftMotorUp = 5;
+    public static int liftMotorDown = 6;
+    public static int Servo = 7;
+    public static int Shooter = 8;
     // public static int leftMotor = 1;
     // public static int rightMotor = 2;
     
@@ -15,4 +19,9 @@ public class RobotMap {
     // number and the module. For example you with a rangefinder:
     // public static int rangefinderPort = 1;
     // public static int rangefinderModule = 1;
+	
+// Aquire ports
+	
+	int TALON_AQUIRE = 8;
+	}
 }
